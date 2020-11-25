81fc1c078e92ff57dce893174279b940b4422f6f
Simone <orco7@disroot.org>
2020-11-25 17:11:14 +0100

    additional commits

--- logs/logs.sh
+++ logs/logs.sh
@@ -3,3 +3,5 @@
 sudo journalctl -xr > '/home/simone/sys_maint/logs/logfull.txt'
 sudo journalctl -xb 0 > '/home/simone/sys_maint/logs/log_since_boot.txt'
 sudo journalctl -xb -1 > '/home/simone/sys_maint/logs/log_last_boot.txt'
+sudo journalctl -p 3 -xb -1 '/home/simone/sys_maint/logs/error_log_last_boot.txt'
+sudo journalctl -p 3 -xb 'home/simone/sys_maint/logs/error_log.txt'
---
+++ pacman/pkg_list.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -Qqe > 'home/simone/sys_maint/pacman/pkglist.txt'
