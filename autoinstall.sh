888f0325768709110c316d57c67a60a1e3e82437
simone <simone.martelli@pm.me>
2021-01-06 10:02:52 +0100

    autoinstall

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-for file in /home/simone/sys_maint/systemd_units
-	do
-		ln -s file /etc/systemd/system
-	done
+for file in /home/simone/sys_maint/systemd_units/
+do
+ln -s file /etc/systemd/system
+done
 
+
+for file in /home/simone/sys_maint/systemd_units/*.timer
+do
+systemctl enable --now file
+done
+	
