f86cfa42e5dbcb11e30737bc271d75fea8505761
simone <simone@arch.localdomain>
2021-01-19 17:51:37 +0100

    added autoinstall script

--- autoinstall.sh
+++ autoinstall.sh
@@ -10,4 +10,8 @@ for file in /home/simone/sys_maint/systemd_units/*.timer
 do
 systemctl enable --now file
 done
-	
+
+ln -s ~/.config/.mpd ~/.mpd
+ln -s ~/.config/.xinitrc ~/.xinitrc
+ln -s ~/.config/.touchpad /etc/X11/xorg.conf.d/90-touchpad.conf
+paru -S --needed < ~/sys_maint/pacman/pkglist.txt
